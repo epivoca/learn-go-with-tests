35a94c58450f66520c551539b495cf12344ac171
epivoca <artfom02@gmail.com>
2024-03-29 21:29:12 +0300

    Code refactore to make Hello func work with empty name passed

--- hello_test.go
+++ hello_test.go
@@ -3,11 +3,26 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris!"
+	// Sometimes it is useful to group tests around a "thing" and then have subtests describing different scenarios.
+	// A benefit of this approach is you can set up shared code that can be used in the other tests.
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, Wofrld!"
+		assertCorrectMessage(t, got, want)
+	})
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// t.Helper() is needed to tell the test suite that this method is a helper.
+	// By doing this when it fails the line number reported will be in our function call (lines 11 and 16)
+	// rather than inside our test helper (line 26).
+	t.Helper()
 	if got != want {
-		// For tests %q is very useful as it wraps your values in double quotes.
-		t.Errorf("got %q, want %q", got, want)
+		t.Errorf("got %q want %q", got, want)
 	}
 }
