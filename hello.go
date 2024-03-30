0700494b45a6f9ad059123c4b386e19e219fa85f
epivoca <artfom02@gmail.com>
2024-03-30 11:34:42 +0300

    Add support for several languages for hello func

--- hello.go
+++ hello.go
@@ -2,18 +2,28 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+var helloPrefixTranslation map[string]string = map[string]string {
+    "ru": "Привет, ",
+    "en": "Hello, ",
+    "es": "Hola, ",
+}
 
-func Hello(name string) string {
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name + "!"
+
+	if helloPrefix, helloPrefixExist := helloPrefixTranslation[language]; helloPrefixExist {
+		return helloPrefix + name + "!"
+	}
+
+	return helloPrefixTranslation["en"] + name + "!"
 }
 
 func main() {
 	// It is good to separate your "domain" code from the outside world (side-effects).
  	// The fmt.Println is a side effect (printing to stdout) and the string we send in is our domain.
   	// So let's separate these concerns so it's easier to test
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
