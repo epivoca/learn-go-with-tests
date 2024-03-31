4c25081f32cb5e833c7e860d665b8bf3027e3245
epivoca <artfom02@gmail.com>
2024-03-31 22:23:03 +0300

    Add comment with reference for standard lib method for repeating strings

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,5 +1,6 @@
 package iteration
 
+// strings.Repeat(character, repeatCount) is standard lib solution for that task
 func Repeat(character string, repeatCount int) string {
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
