c62a80dc258b32b00490a9d87f7d8c667dc628dc
epivoca <artfom02@gmail.com>
2024-05-01 10:14:26 +0300

    Change table implementation from slice to map and refactor Perimeter func as method

--- structs/shapes.go
+++ structs/shapes.go
@@ -4,6 +4,7 @@ import "math"
 
 type Shape interface {
 	Area() float64
+	Perimeter() float64
 }
 
 type Rectangle struct {
@@ -15,13 +16,23 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
 type Triangle struct {
 	Base   float64
 	Height float64
+	SideA  float64
+	SideB  float64
 }
 
 func (t Triangle) Area() float64 {
 	return 0.5 * t.Base * t.Height
+}
+
+func (t Triangle) Perimeter() float64 {
+	return t.Base + t.SideB + t.SideA
 }
 
 type Circle struct {
@@ -32,6 +43,6 @@ func (c Circle) Area() float64 {
 	return math.Pi * math.Pow(c.Radius, 2)
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -5,36 +5,60 @@ import (
 )
 
 func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
-	want := 40.0
+	perimeterTests := map[string]struct {
+		shape        Shape
+		hasPerimeter float64
+	}{
+		"Rectangle": {
+			shape:        Rectangle{Width: 12, Height: 6},
+			hasPerimeter: 36.0,
+		},
+		"Circle": {
+			shape:        Circle{Radius: 10.0},
+			hasPerimeter: 62.83185307179586,
+		},
+		"Triangle": {
+			shape:        Triangle{Base: 10.0, Height: 7.0, SideA: 4.0, SideB: 8.0},
+			hasPerimeter: 22.0,
+		},
+	}
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	for name, tt := range perimeterTests {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			if got, expected := tt.shape.Perimeter(), tt.hasPerimeter; got != expected {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasPerimeter)
+			}
+		})
 	}
 }
 
 func TestArea(t *testing.T) {
-
-	areaTests := []struct {
-		name    string
+	areaTests := map[string]struct {
 		shape   Shape
 		hasArea float64
 	}{
-		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
-		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
-		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 6.0},
+		"Rectangle": {
+			shape:   Rectangle{Width: 12, Height: 6},
+			hasArea: 72.0,
+		},
+		"Circle": {
+			shape:   Circle{Radius: 10.0},
+			hasArea: 314.1592653589793,
+		},
+		"Triangle": {
+			shape:   Triangle{Base: 12, Height: 6},
+			hasArea: 36,
+		},
 	}
 
-	for _, tt := range areaTests {
-		// using tt.name from the case to use it as the `t.Run` test name
-		t.Run(tt.name, func(t *testing.T) {
-			got := tt.shape.Area()
-			if got != tt.hasArea {
+	for name, tt := range areaTests {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			if got, expected := tt.shape.Area(), tt.hasArea; got != expected {
 				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
 			}
 		})
-
 	}
-
 }
