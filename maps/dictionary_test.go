ebf2ccecea352c378fb93f4e33b58cb2913251ce
epivoca <artfom02@gmail.com>
2024-10-27 18:26:21 +0300

    Extract strings comparison into a method to make implementation more general

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -4,13 +4,20 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	word := "test"
-	definition := "what is test, actually? in terms of programming, I guess, it's just a way to make sure, that your code runs properly"
+	definition := "google.com"
+
 	dictionary := map[string]string{word: definition}
 
 	got := Search(dictionary, word)
 	want := definition
 
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
 	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, dictionary)
+		t.Errorf("got %q want %q", got, want)
 	}
 }
