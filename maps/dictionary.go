4ffa31301f7a4fda15c3661fa797c034a7018379
epivoca <artfom02@gmail.com>
2025-01-02 22:04:06 +1100

    Change Search() behaviour to return emtry string and error if no definition was found

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,9 +1,16 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
 // Search searches for a word in the dictionary
 // and returns its definition
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, definitionExists := d[word]
+	if !definitionExists {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return definition, nil
 }
