0a5abe7a52def2968cd695e1a0348a7fbd2a6124
epivoca <artfom02@gmail.com>
2025-01-03 21:26:47 +1100

    Modify Add method to return ErrWordExists if an word was passed to excluce modifying behavior

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -4,10 +4,13 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("provided word already exists")
+)
 
-// Search searches for a word in the dictionary
-// and returns its definition
+// Search searches for a word in the dictionary and returns its definition
+// or ErrNotFound if no definition was found
 func (d Dictionary) Search(word string) (string, error) {
 	definition, definitionExists := d[word]
 	if !definitionExists {
@@ -17,7 +20,15 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-// Add adds a new word and a definition to the dictionary
-func (d Dictionary) Add(word, definition string) {
+// Add adds a new word and a definition to the dictionary and returns ErrWordExists
+// if a word already exists
+func (d Dictionary) Add(word, definition string) error {
+	// Must abort Add for existing words because it would have a modifying behaviour which is not
+	// an expected behavior
+	if _, err := d.Search(word); err == nil {
+		return ErrWordExists
+	}
+
 	d[word] = definition
+	return nil
 }
