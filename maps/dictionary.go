5856169e71a4587d59c0de5e2c7d1c2600c2332d
epivoca <artfom02@gmail.com>
2025-01-03 21:37:08 +1100

    Change check for existing words from condition to switch operator
    It is a better approach since we won't let errors other than ErrNotFound to pass through

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -23,12 +23,21 @@ func (d Dictionary) Search(word string) (string, error) {
 // Add adds a new word and a definition to the dictionary and returns ErrWordExists
 // if a word already exists
 func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
 	// Must abort Add for existing words because it would have a modify them which is not
 	// an expected behavior
-	if _, err := d.Search(word); err == nil {
+	//
+	// Having a switch like this provides an extra safety net,
+	// in case Search returns an error other than ErrNotFound.
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
 		return ErrWordExists
+	default:
+		return err
 	}
 
-	d[word] = definition
 	return nil
 }
